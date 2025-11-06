924d5dd7991e25b84cc16bf5c5db8b91823945bd
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2025-11-06 16:07:33 +0200

    Fix formatting of schema export comments

--- core/schema/index.ts
+++ core/schema/index.ts
@@ -5,5 +5,5 @@
 export * from './agent.schema.js';
 export { default as AgentConfigSchema } from './agent.schema.js';
 export * from './primitives.schema.js';
-export { default as Primitives } from './primitives.schema.js';
 export * from './registry-schema-builder.js';
+
